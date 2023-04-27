1ae8aabb3398059b2183159f189ae52b7c6129a6
GGG <GGG@DESKTOP-8N3GG4O>
2023-04-27 12:47:51 +0900

    ㅋㅋ

---
+++ magicWebApp/src/magic/copy/copyDBBean.java
@@ -0,0 +1,24 @@
+package magic.copy;
+
+import java.sql.Connection;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+public class copyDBBean {
+	//이 클래스의 인스턴스를 오직 하나만 생성하고, 여러곳에서 참조할 수 있게 함
+	//한 번만 생성해도 여러곳에서 참조가능  = static 
+	//외부 접근을 막음=>private
+	//=> 이게 바로 싱글톤 디자인패턴
+	private static copyDBBean instance = new copyDBBean();
+	
+	//외부 접근을 막았으므로,이 클래스의 instance를 사용하려면 public 메소드가 필요함.
+	public static copyDBBean getInstance() {
+		return instance;
+	}
+	
+	public Connection getConnection() throws Exception{
+		Context ctx = new InitialContext();
+		data
+	}
+}
